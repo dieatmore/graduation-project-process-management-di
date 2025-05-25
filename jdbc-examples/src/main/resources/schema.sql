0caf8c95ae19429a708adccf8010803529fdae6b
dieatmore <s.d.wang.1@nefu.edu.cn>
2025-05-25 15:30:45 +0800

    修改数据库

--- jdbc-examples/src/main/resources/schema.sql
+++ jdbc-examples/src/main/resources/schema.sql
@@ -14,12 +14,14 @@ create table if not exists `user`
     name          varchar(20)      not null,
     department_id char(19)         not null,
     role          varchar(20)      not null,
-    student       json             comment '{"year", "teacherId", "projectTitle"}',
-    teacher       json             comment '{"teachNumber"}',
+    group_number  tinyint unsigned null,
+    student       json             null comment '{"teacherId", "projectTitle", "group_order"， "auth"}', -- 添加组内顺序，自动生成后填入
+    teacher       json             null comment '{"teachNumber", "A", "C", "auth"}', -- 添加A,C组, 分组后填入
     insert_time   datetime         not null default current_timestamp,
     update_time   datetime         not null default current_timestamp on update current_timestamp,
 
-    index((cast( student ->> '$.teacherId' as char(19)) collate utf8mb4_bin))
+    index((cast( student ->> '$.teacherId' as char(19)) collate utf8mb4_bin)), -- 搜索指导老师的学生
+    index(department_id, group_number, role)  -- 查询某个专业的第几组的老师或者学生
 );
 
 create table if not exists `process`
@@ -29,26 +31,12 @@ create table if not exists `process`
     items           json               comment '[{"number", "name", "point", "description"}]',
     proportion      varchar(20)        not null,
     department_id   char(19)           not null,
-    year            year               not null,
-    file_path       varchar(20),
+    auth            varchar(10)        not null,  -- 每个过程定义对应权限
+    file            json               null comment '[{"id", "name", "type", "path"}]',
     insert_time     datetime           not null default current_timestamp,
     update_time     datetime           not null default current_timestamp on update current_timestamp,
 
-    unique(department_id, year)
-);
-
-create table if not exists 'process_group'
-(
-    id               char(19)       not null primary key,
-    process_id       char(19)       not null,
-    teacher_id       char(19)       not null,
-    student_id       char(19)       not null,
-    order            varchar(20)    not null,
-    insert_time      datetime       not null default current_timestamp,
-    update_time      datetime       not null default current_timestamp on update current_timestamp,
-
-    index(teacher_id),
-    index(student_id)
+    index(department_id, (cast( items ->> '$.number' as char(19)) collate utf8mb4_bin)) -- 查询专业和查询专业的的过程子项
 );
 
 create table if not exists `process_score`
@@ -61,5 +49,5 @@ create table if not exists `process_score`
     insert_time   datetime    not null default current_timestamp,
     update_time   datetime    not null default current_timestamp on update current_timestamp,
 
-    unique(student_id, process_id, teacher_id)
-);
+    unique(process_id, student_id, teacher_id) -- 从左到右，先查过程ID,再查学生ID，通过user表确定第几组和其老师，确定由哪个老师打分
+);                                             -- 查询某个过程的所有学生的分数
